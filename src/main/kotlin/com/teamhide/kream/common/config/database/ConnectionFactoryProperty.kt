bcca7dffc98da2bcd0d6731435ec0b08388bda96
Hide <padocon@naver.com>
2024-08-13 22:00:16 +0900

    [#7] Refactor RoutingConnectionFactoryConfig

--- src/main/kotlin/com/teamhide/kream/common/config/database/ConnectionFactoryProperty.kt
+++ src/main/kotlin/com/teamhide/kream/common/config/database/ConnectionFactoryProperty.kt
@@ -2,34 +2,18 @@ package com.teamhide.kream.common.config.database
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-interface ConnectionFactoryProperty {
-    val driver: String
-    val protocol: String
-    val host: String
-    val port: Int
-    val user: String
-    val password: String
-    val database: String
+@ConfigurationProperties(prefix = "spring.r2dbc")
+data class ConnectionFactoryProperties(
+    val writer: ConnectionFactoryProperty,
+    val reader: ConnectionFactoryProperty,
+) {
+    data class ConnectionFactoryProperty(
+        val driver: String,
+        val protocol: String,
+        val host: String,
+        val port: Int,
+        val user: String,
+        val password: String,
+        val database: String,
+    )
 }
-
-@ConfigurationProperties(prefix = "spring.r2dbc.writer")
-data class WriterConnectionFactoryProperty(
-    override val driver: String,
-    override val protocol: String,
-    override val host: String,
-    override val port: Int,
-    override val user: String,
-    override val password: String,
-    override val database: String,
-) : ConnectionFactoryProperty
-
-@ConfigurationProperties(prefix = "spring.r2dbc.reader")
-data class ReaderConnectionFactoryProperty(
-    override val driver: String,
-    override val protocol: String,
-    override val host: String,
-    override val port: Int,
-    override val user: String,
-    override val password: String,
-    override val database: String,
-) : ConnectionFactoryProperty
--- src/main/kotlin/com/teamhide/kream/common/config/database/RoutingConnectionFactoryConfig.kt
+++ src/main/kotlin/com/teamhide/kream/common/config/database/RoutingConnectionFactoryConfig.kt
@@ -1,9 +1,7 @@
 package com.teamhide.kream.common.config.database
 
-import io.asyncer.r2dbc.mysql.MySqlConnectionFactoryProvider
 import io.r2dbc.spi.ConnectionFactories
 import io.r2dbc.spi.ConnectionFactory
-import io.r2dbc.spi.ConnectionFactoryOptions
 import io.r2dbc.spi.ConnectionFactoryOptions.DATABASE
 import io.r2dbc.spi.ConnectionFactoryOptions.DRIVER
 import io.r2dbc.spi.ConnectionFactoryOptions.HOST
@@ -11,6 +9,7 @@ import io.r2dbc.spi.ConnectionFactoryOptions.PASSWORD
 import io.r2dbc.spi.ConnectionFactoryOptions.PORT
 import io.r2dbc.spi.ConnectionFactoryOptions.PROTOCOL
 import io.r2dbc.spi.ConnectionFactoryOptions.USER
+import io.r2dbc.spi.ConnectionFactoryOptions.builder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration
@@ -18,44 +17,40 @@ import org.springframework.r2dbc.connection.R2dbcTransactionManager
 import org.springframework.r2dbc.connection.TransactionAwareConnectionFactoryProxy
 import org.springframework.transaction.ReactiveTransactionManager
 import org.springframework.transaction.annotation.EnableTransactionManagement
-import java.time.ZoneId
 
 @Configuration
 @EnableTransactionManagement
 class RoutingConnectionFactoryConfig(
-    private val writerConnectionFactoryProperty: WriterConnectionFactoryProperty,
-    private val readerConnectionFactoryProperty: ReaderConnectionFactoryProperty,
+    private val connectionFactoryProperties: ConnectionFactoryProperties,
 ) : AbstractR2dbcConfiguration() {
     @Bean
     override fun connectionFactory(): ConnectionFactory {
-        val routingConnectionFactory = RoutingConnectionFactory()
-        val writerConnectionFactory = writerConnectionFactory()
-        val readerConnectionFactory = readerConnectionFactory()
-        val connectionFactoryMap = mapOf(
-            ConnectionFactoryType.WRITER to writerConnectionFactory,
-            ConnectionFactoryType.READER to readerConnectionFactory,
+        val readerConnectionFactory = createConnectionFactory(
+            property = connectionFactoryProperties.reader,
         )
-
-        routingConnectionFactory.setTargetConnectionFactories(connectionFactoryMap)
-        routingConnectionFactory.setDefaultTargetConnectionFactory(writerConnectionFactory)
-
+        val writerConnectionFactory = createConnectionFactory(
+            property = connectionFactoryProperties.writer,
+        )
+        val routingConnectionFactory = RoutingConnectionFactory().apply {
+            setTargetConnectionFactories(
+                mapOf(
+                    ConnectionFactoryType.WRITER to readerConnectionFactory,
+                    ConnectionFactoryType.READER to writerConnectionFactory,
+                )
+            )
+            setDefaultTargetConnectionFactory(readerConnectionFactory)
+        }
         return routingConnectionFactory
     }
-
-    @Bean
-    fun writerConnectionFactory(): ConnectionFactory = makeConnectionFactory(property = writerConnectionFactoryProperty)
-
-    @Bean
-    fun readerConnectionFactory(): ConnectionFactory = makeConnectionFactory(property = readerConnectionFactoryProperty)
 
     @Bean
     fun reactiveTransactionManager(connectionFactory: ConnectionFactory): ReactiveTransactionManager {
         return R2dbcTransactionManager(TransactionAwareConnectionFactoryProxy(connectionFactory))
     }
 
-    private fun makeConnectionFactory(property: ConnectionFactoryProperty): ConnectionFactory {
+    private fun createConnectionFactory(property: ConnectionFactoryProperties.ConnectionFactoryProperty): ConnectionFactory {
         return ConnectionFactories.get(
-            ConnectionFactoryOptions.builder()
+            builder()
                 .option(DRIVER, property.driver)
                 .option(PROTOCOL, property.protocol)
                 .option(HOST, property.host)
@@ -63,7 +58,6 @@ class RoutingConnectionFactoryConfig(
                 .option(USER, property.user)
                 .option(PASSWORD, property.password)
                 .option(DATABASE, property.database)
-                .option(MySqlConnectionFactoryProvider.SERVER_ZONE_ID, ZoneId.of("Asia/Seoul"))
                 .build()
         )
     }
