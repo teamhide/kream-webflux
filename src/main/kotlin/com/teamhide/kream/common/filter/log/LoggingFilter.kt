1bc6fa60934c0ee63f64a02d4aff158080d53f87
Hide <padocon@naver.com>
2024-08-15 22:46:40 +0900

    [#11] LoggingFilter replace WebFlilter to CoWebFilter

--- src/main/kotlin/com/teamhide/kream/common/filter/log/LoggingFilter.kt
+++ src/main/kotlin/com/teamhide/kream/common/filter/log/LoggingFilter.kt
@@ -3,15 +3,14 @@ package com.teamhide.kream.common.filter.log
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
+import org.springframework.web.server.CoWebFilter
+import org.springframework.web.server.CoWebFilterChain
 import org.springframework.web.server.ServerWebExchange
-import org.springframework.web.server.WebFilter
-import org.springframework.web.server.WebFilterChain
-import reactor.core.publisher.Mono
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-class LoggingFilter : WebFilter {
-    override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
+class LoggingFilter : CoWebFilter() {
+    override suspend fun filter(exchange: ServerWebExchange, chain: CoWebFilterChain) {
         return chain.filter(LoggingDecorator(exchange = exchange))
     }
 }
